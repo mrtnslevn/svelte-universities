a0b448964d42cba467e67bac4a4ff9de319639b0
Martinus Rexyfarrel Evan Sayoga <martinusevan3@gmail.com>
2023-07-31 16:17:22 +0700

    commit new

---
+++ src/lib/charts/Charts.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { onMount } from 'svelte';
+  import Chart from 'chart.js/auto';
+
+  /**
+   * @type {any}
+   */
+   export let chartData;
+
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  let canvas;
+
+  let myChart;
+
+  onMount(() => {
+    const labels = ['Januari', 'Februari', 'Maret', 'April', 'Mei'];
+    const ctx = canvas.getContext('2d');
+    // @ts-ignore
+    myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Data ',
+          data: chartData,
+          backgroundColor: 'rgba(75, 192, 192, 0.6)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+        }],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  });
+</script>
+
+<canvas bind:this={canvas}></canvas>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Navbar from '$lib/nav/Navbar.svelte';
+    import '../app.css'
+
+    export const ssr=false;
+</script>
+
+<Navbar />
+<slot />
