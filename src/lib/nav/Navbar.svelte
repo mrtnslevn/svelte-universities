7c7368f113d389780d0e1b4b411dd846e5635cbc
Martinus Rexyfarrel Evan Sayoga <martinusevan3@gmail.com>
2023-08-01 12:18:50 +0700

    Update Navbar.svelte

--- src/lib/nav/Navbar.svelte
+++ src/lib/nav/Navbar.svelte
@@ -23,9 +23,9 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             
-            <a href="/hundred" class="hover:bg-gray-700 text-gray-300 hover:text-white rounded-md px-3 py-2 text-sm font-medium">100 Data</a>
-            <a href="/thousand" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">1000 Data</a>
-            <a href="/tenThousand" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">10000 Data</a>
+            <a href="/hundred" class="hover:bg-gray-700 text-gray-300 hover:text-white rounded-md px-3 py-2 text-sm font-medium">10 Data</a>
+            <a href="/thousand" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">100 Data</a>
+            <a href="/tenThousand" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">1000 Data</a>
             <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
           </div>
         </div>
